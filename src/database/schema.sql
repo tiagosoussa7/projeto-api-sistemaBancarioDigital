ae7b7bfb1babebedb3c1a3d4b5589dc00a1501ab
tiagosoussa7 <tiago.soussa@live.com>
2024-01-17 08:42:17 -0300

    correção da tabela dados_banco e envio da tabela dados_cliente

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -3,7 +3,18 @@ create database banco;
 create table dados_banco (
   id_banco serial primary key,
   nome text not null,
-  senha varchar(25) not null,
-  qtd_contas decimal(10,2) default 0,
-  data_fundacao timestamp default current_timestamp
+  qtd_contas decimal(10, 2) default 0,
+  orcamento decimal(10, 2) default 0,
+  data_fundacao timestamp default current_timestamp,
+  senha varchar(255) not null
+ );
+  
+  create table dados_cliente (
+    id_cliente serial primary key,
+    nome text not null,
+    cpf varchar(11) unique not null,
+    email varchar(100) unique not null,
+    data_nascimento date not null,
+    senha varchar(255) not null,
+    id_banco int references dados_banco(id_banco) on delete cascade
   );
