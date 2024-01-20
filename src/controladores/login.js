658cc595e276a41d0a13e26b2e25522377141714
tiagosoussa7 <tiago.soussa@live.com>
2024-01-20 14:57:45 -0300

    Refatoração do código de login.

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -1,106 +1,52 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const knex = require("../conexoes/knex");
+const { gerar_token } = require("../validacoes/schema_login");
 const { nome_resposta } = require('../validacoes/schema_resposta');
 
-const login_banco = async (req, res) => {
-    const { nome, senha} = req.body;
-
-    if (!nome || !senha) return res.status(400).json({mensagem: 'Os campos nome e senha são obrigatórios'});
+const login = async (req, res) => {
+    const { instituicao_nome, cpf, email, senha } = req.body;
 
     try {
-        const banco_cadastrado = await knex('dados_banco').select('nome', 'senha').first();
-        
-        if (banco_cadastrado.nome !== nome) return res.status(400).json({mensagem: `O nome ${nome} não corresponde ao do banco que está cadastrado no sistema.`});
-        
-        const senha_correta = await bcrypt.compare(senha, banco_cadastrado.senha);
-
-        if (!senha_correta) return res.status(400).json({mensagem: 'Senha inválida.'});
-
-        const token = jwt.sign({nome: banco_cadastrado.nome}, 'senha_segura_token', {expiresIn: '10h'});
-
-        return res.status(200).json({token: token});
-    
-    } catch (error) {
-        return res.status(500).json({mensagem: `${error.message}`});
-    }
-}
-
-
-const login_geral = async (req, res) => {
-    const { instituicao_nome, instituicao_senha, cpf, email, senha } = req.body;
-
-    try {
-        if (instituicao_nome && instituicao_senha) {
+        if (instituicao_nome && senha) {
             
             const banco_cadastrado = await knex('dados_banco').select('nome', 'senha').first();
             
-            if (!banco_cadastrado) return res.status(400).json({mensagem: 'Não existe instituição cadastrada no sistema'});
+            if (!banco_cadastrado) return res.status(400).json({mensagem: 'Login negado: não existe instituição cadastrada no sistema'});
 
-            if (banco_cadastrado.nome !== instituicao_nome) return res.status(400).json({mensagem: `O nome ${nome_resposta(instituicao_nome)} não corresponde ao do banco que está cadastrado no sistema.`});
-        
-            const banco_senha = await bcrypt.compare(instituicao_senha, banco_cadastrado.senha);
-
-            if (!banco_senha) return res.status(400).json({mensagem: 'Senha inválida.'});
-
-            const token = jwt.sign({senha: banco_cadastrado.senha}, 'senha_segura_token', {expiresIn: '10h'});
-
-            return res.status(200).json({token: token});
+            if (banco_cadastrado.nome !== instituicao_nome) return res.status(400).json({mensagem: `Login negado: o nome ${nome_resposta(instituicao_nome)} não corresponde ao do banco cadastrado no sistema.`});
+            
+            return gerar_token(banco_cadastrado, senha, res);
         }
         
-        if (cpf || email && senha) {
+        if (cpf && senha || email && senha) {
             
             if (cpf) {
                 
                 const cpf_cadastrado = await knex('dados_cliente').where({cpf}).first();
 
-                if (!cpf_cadastrado) {
-                    return res.status(400).json({mensagem: `Login negado: o CPF ${cpf} não foi encontrado.`}); 
-                }
-                
-                const cliente_senha = await bcrypt.compare(senha, cpf_cadastrado.senha);
-
-                if (!cliente_senha) return res.status(400).json({mensagem: 'Senha inválida.'});
-
-                const token = jwt.sign({senha: cpf_cadastrado.senha}, 'senha_segura_token', {expiresIn: '10h'});
-
-                return res.status(200).json({token: token});
+                if (!cpf_cadastrado) return res.status(400).json({mensagem: `Login negado: o CPF ${cpf} não foi encontrado.`}); 
+                                
+                return gerar_token(cpf_cadastrado, senha, res);
             }
             
             if(email) {
                 const email_cadastrado = await knex('dados_cliente').where({email}).first();
 
-                if (!email_cadastrado) {
-                    return res.status(400).json({mensagem: `Login negado: o email ${email} não foi encontrado.`}); 
-                }
-
-                const cliente_senha = await bcrypt.compare(senha, email_cadastrado.senha);
-
-                if (!cliente_senha) return res.status(400).json({mensagem: 'Senha inválida.'});
-
-                const token = jwt.sign({senha: email_cadastrado.senha}, 'senha_segura_token', {expiresIn: '10h'});
-
-                return res.status(200).json({token: token});
+                if (!email_cadastrado) return res.status(400).json({mensagem: `Login negado: o email ${email} não foi encontrado.`}); 
+                
+                return gerar_token(email_cadastrado, senha, res);
             }
-            
-
-            //const cliente_senha = await bcrypt.compare(senha, `${cpf_cadastrado ? cpf_cadastrado.senha : email_cadastrado.senha}`);
-
-            //if (!cliente_senha) return res.status(400).json({mensagem: 'Senha inválida.'});
-
-            //const token = jwt.sign({senha: `${cpf_cadastrado ? cpf_cadastrado.senha : email_cadastrado.senha}`}, 'senha_segura_token', {expiresIn: '10h'});
-
-            //return res.status(200).json({token: token});
         }
-        
-        return res.status(400).json({mensagem: 'Todos os campos são obrigatórios.'});
+                
+        return res.status(400).json({mensagem: 'Login negado: todos os campos são obrigatórios.'});
 
     } catch (error) {
         return res.status(500).json({mensagem: `${error.message}`});
     }
 }
-
+            
 module.exports = {
-    login_banco,
-    login_geral
+    login
 }
+
+            
+        
