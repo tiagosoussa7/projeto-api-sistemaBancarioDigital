6900d4beb792ae431f93ab52ffcd7679f27bd8f5
tiagosoussa7 <tiago.soussa@live.com>
2024-01-21 11:47:01 -0300

    Adequação de rotas para novo código de consulta de dados do cliente.

--- src/rotas/rotas_banco.js
+++ src/rotas/rotas_banco.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { cadastro, atualizar, informacoes, excluir, consulta_conta } = require('../controladores/banco');
+const { cadastro, atualizar, informacoes, excluir, consulta_conta, consulta_cliente } = require('../controladores/banco');
 const { banco_autenticacao } = require('../intermediarios/autenticacao');
 
 const rota_banco = express();
@@ -8,7 +8,8 @@ rota_banco.post('/banco', cadastro);
 
 rota_banco.use(banco_autenticacao);
 rota_banco.get('/banco/informacao', informacoes);
-rota_banco.get('/banco/consultar', consulta_conta);
+rota_banco.get('/banco/consultar/conta', consulta_conta);
+rota_banco.get('/banco/consultar/cliente', consulta_cliente);
 rota_banco.put('/banco', atualizar);
 rota_banco.delete('/banco', excluir);
 
