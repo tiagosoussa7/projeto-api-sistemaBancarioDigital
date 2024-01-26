2c9262374b45657c5cb8c35252643a16e564a87c
tiagosoussa7 <tiago.soussa@live.com>
2024-01-26 13:49:54 -0300

    Melhorias na organização do projeto.

---
+++ src/util/conta/util_extrato.js
@@ -0,0 +1,56 @@
+const knex = require("../../conexoes/knex");
+const { data_resposta, hora_resposta } = require("../util_resposta");
+
+async function detalhar_depositos(tabela, cliente, res) {
+    return res.status(200).json({Extrato_depositos: await extrato_geral(tabela, cliente)});
+}
+
+async function detalhar_saques(tabela, cliente, res) {
+    return res.status(200).json({Extrato_saques: await extrato_geral(tabela, cliente)});
+}
+
+async function detalhar_transferencias(cliente, res) {
+    
+    const enviados = await knex('dados_transferencias').where({numero_conta: cliente.id_cliente}).select('*');
+    const recebidos = await knex('dados_transferencias').where({conta_destino: cliente.id_cliente}).select('*');
+            
+            const dadosEnviados = enviados.map((enviado) => ({
+                Conta_destino: enviado.conta_destino,
+                Valor: enviado.valor,
+                Data: data_resposta(enviado.data),
+                Horário: hora_resposta(enviado.hora)
+            }));
+            
+            const dadosRecebidos = recebidos.map((recebido) => ({
+                Conta_origem: recebido.numero_conta,
+                Valor: recebido.valor,
+                Data: data_resposta(recebido.data),
+                Horário: hora_resposta(recebido.hora)
+            }));
+   
+    return res.status(200).json({
+        Transferências_enviadas: dadosEnviados,
+        Transferências_recebidas: dadosRecebidos,
+    });
+}
+
+async function extrato_geral(tabela, cliente) {
+    
+    const dados = await knex(tabela).where({numero_conta: cliente.id_cliente}).select('*');
+            
+            const dadosFormatados = dados.map((dado) => ({
+                Numero_conta: dado.numero_conta,
+                Valor: dado.valor,
+                Data: data_resposta(dado.data),
+                Horário: hora_resposta(dado.hora)
+            }));
+
+    return dadosFormatados;        
+}
+
+
+module.exports = {
+    detalhar_depositos,
+    detalhar_saques,
+    detalhar_transferencias
+}
