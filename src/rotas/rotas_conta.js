b1aac4eb7a02811c0b6cc1e3210909a3178768e8
tiagosoussa7 <tiago.soussa@live.com>
2024-01-26 13:53:55 -0300

    Envio da rota de extrato conta.

--- src/rotas/rotas_conta.js
+++ src/rotas/rotas_conta.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { cadastro, informacao_cliente, informacao_conta, atualizar, excluir } = require('../controladores/conta');
-const { autenticacaoCliente } = require('../intermediarios/autenticacao.js');
+const { cadastro, informacao_cliente, informacao_conta, atualizar, excluir, extrato } = require('../controladores/conta');
+const { autenticacaoCliente, autenticacaoBanco } = require('../intermediarios/autenticacao.js');
 
 const rota_conta = express();
 
-rota_conta.post('/conta', cadastro);
+rota_conta.post('/conta', autenticacaoBanco, cadastro);
 
 rota_conta.use(autenticacaoCliente);
 rota_conta.get('/conta/informacao', informacao_conta);
 rota_conta.get('/conta/informacao/cliente', informacao_cliente);
+rota_conta.get('/conta/extrato', extrato);
 rota_conta.put('/conta', atualizar);
 rota_conta.delete('/conta', excluir);
 
